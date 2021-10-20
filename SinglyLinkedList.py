2dc09f34b97b84309806984ff7984835faa4dd88
shatheesh17 <36799558+shatheesh17@users.noreply.github.com>
2021-10-20 13:01:20 +0530

    insertion in sll

--- SinglyLinkedList.py
+++ SinglyLinkedList.py
@@ -1,17 +1,53 @@
+class Node:
+    def __init__(self,value=None):
+        self.value =value
+        self.next=None
+
 class SLinkedList:
     def __init__(self):
         self.head=None
         self.tail=None
 
-class Node:
-    def __init__(self,value):
-        self.value =value
-        self.next=None
+    def __iter__(self):
+        node=self.head
+        while node:
+            yield node
+            node=node.next
+
+    def insertSll(self,value,location=0):
+        newNode=Node(value)
+        #Check if any element exist
+        if self.head==None:
+            self.head=newNode
+            self.tail=newNode
+            return
+        #Beginning
+        if location==0:
+            newNode.next=self.head
+            self.head=newNode
+        #End
+        elif location==1:
+            newNode.next=None
+            self.tail.next=newNode
+            self.tail=newNode
+        #Middle
+        else:
+            tempNode=self.head
+            index=0
+            while index<location-1:
+                tempNode=tempNode.next
+                index+=1
+
+            newNode.next=tempNode.next
+            tempNode.next=newNode
+
 
 sll=SLinkedList()
-node1=Node(1)
-node2=Node(2)
-sll.head=node1
-node1.next=node2
-sll.tail=node2
-#print(sll.head.value)
+sll.insertSll(2)
+sll.insertSll(1)
+sll.insertSll(3,1)
+sll.insertSll(5,1)
+sll.insertSll(4,3)
+
+
+print([node.value for node in sll])
